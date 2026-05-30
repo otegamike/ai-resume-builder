f3a1d81b01935ace42cd918144a865edf73d368a
Mike Imi Otega <otegamike111@gmail.com>
2026-05-30 15:09:25 +0100

    Configure turbopack in next.config.ts
    Added turbopack configuration to next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['172.20.10.3', '172.20.10.4' ],
+  allowedDevOrigins: ['172.20.10.3', '172.20.10.4'],
   serverExternalPackages: ["@sparticuz/chromium", "playwright-core"],
+  turbopack: {
+    // disable turbopack bundling for these modules
+    resolveAlias: {
+      "playwright-core": "playwright-core",
+      "@sparticuz/chromium": "@sparticuz/chromium",
+    },
+  },
 };
 
 export default nextConfig;
